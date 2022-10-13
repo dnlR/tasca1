69f0877e7c047b38e31532aed96dbd31b866b375
ddnlr <romerodaniel@protonmail.com>
2022-10-13 10:26:28 +0200

    Include www in gitignore

--- www/classes/holyday.js
+++
@@ -1,9 +0,0 @@
-export class Holyday {
-    constructor(name, price, country, description) {
-        this.name = name;
-        this.price = price;
-        this.country = country;
-        this.description = description;
-    }
-}
-//# sourceMappingURL=holyday.js.map
--- www/interfaces/isholyday.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=isholyday.js.map
